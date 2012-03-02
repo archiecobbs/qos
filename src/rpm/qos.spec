d4b928ed2844f27e226563651c9e0ec6135a885b
Archie L. Cobbs <archie@dellroad.org>
2012-03-02 15:06:30 -0600

    Initial commit.

---
+++ src/rpm/qos.spec
@@ -0,0 +1,83 @@
+
+%define name        qos
+%define svcname     %{name}
+
+%define initdir     %{_sysconfdir}/init.d
+%define netconfdir  %{_sysconfdir}/sysconfig/network
+%define qosconfig   %{netconfdir}/%{svcname}
+%define modprobe    /sbin/modprobe
+%define tcutil      %{_sbindir}/tc
+%define iputil      /bin/ip
+%define fillupdir   %{_var}/adm/fillup-templates
+
+Name:               %{name}
+Version:            1.0.%{svn_revision}
+Release:            1
+Buildarch:          noarch
+Summary:            A Simple Linux QoS Service
+Group:              System/Management
+License:            Apache License, Version 2.0
+Source0:            %{name}.zip
+BuildRoot:          %{_tmppath}/%{name}-root
+URL:                https://github.com/archiecobbs/qos
+Requires:           iproute2
+Requires(post):     fillup
+
+%description
+qos: A Simple Linux QoS Service
+
+The Problem: Bufferbloat (see http://en.wikipedia.org/wiki/Bufferbloat)
+
+    - Your SSH session turns to molasses when your kid watches YouTube
+    - Your wife complains that "the internet is slow"
+    - You hate the stupid DSL modems supplied by the phone company
+      with their giant packet queues that add unnecessary latency
+    - You have your own Linux router that routes all your traffic
+      or is the only machine you have connected to the Internet
+      and know there must be a better way
+
+The Solution: QoS
+
+    QoS = "Quality of Service"
+
+    You probably already know about it. Control and proritize traffic.
+
+    This QoS is new and improved. Previous QoS setups only throttled
+    traffic in the download direction. This one handles both directions
+    using the (poorly documented) Linux ifb interface and tc(8) 'mirred'
+    redirection.
+
+%prep
+rm -rf ${RPM_BUILD_ROOT}
+%setup -c
+
+%build
+subst()
+{
+    sed -r \
+        -e 's|@qosconfig@|%{qosconfig}|g' \
+        -e 's|@modprobe@|%{modprobe}|g' \
+        -e 's|@iputil@|%{iputil}|g' \
+        -e 's|@tcutil@|%{tcutil}|g'
+}
+subst < scripts/%{svcname}.sh > %{svcname}
+
+%install
+
+# Install init script
+install -d -m 0755 ${RPM_BUILD_ROOT}%{initdir}
+install -m 0755 %{svcname} ${RPM_BUILD_ROOT}%{initdir}/
+
+# Install sysconfig template
+install -d -m 0755 ${RPM_BUILD_ROOT}%{fillupdir}
+install -m 0755 fillup/sysconfig.%{svcname} ${RPM_BUILD_ROOT}%{fillupdir}/
+
+%post
+%{fillup_only -n -d %{svcname} network}
+
+%preun
+%{stop_on_removal %{svcname}}
+
+%files
+%attr(0755,root,root) %{initdir}/*
+%attr(0644,root,root) %{fillupdir}/*
---
+++ src/scripts/qos.sh
@@ -0,0 +1,277 @@
+#! /bin/bash
+#
+### BEGIN INIT INFO
+# Provides: qos
+# Required-Start: $network
+# Should-Start:
+# Required-Stop: $network
+# Should-Stop:
+# Default-Start:  3 4 5
+# Default-Stop:   0 1 2 6
+# Short-Description: QoS configuration
+# Description: Configures the kernel for routing traffic prioritization
+### END INIT INFO
+
+#
+# This script sets up QoS prioritization using HTB (Hierarchical Token Bucket).
+# Assumptions:
+#
+#   o Nothing else uses queueing disciplines on the $QOS_DEV_IFB and $QOS_DEV_EXT interfaces
+#
+# References:
+#   http://www.lartc.org
+#   http://tldp.org/HOWTO/ADSL-Bandwidth-Management-HOWTO/
+#
+
+# Constants
+QOSCONFIG='@qosconfig@'
+IP='@iputil@'
+TC='@tcutil@'
+MODPROBE='@modprobe@'
+
+. /etc/rc.status
+rc_reset
+
+# Slurp in configuration
+test -r "$QOSCONFIG" || exit 5
+. "$QOSCONFIG"
+test -n "$QOS_DEV_EXT" || exit 5
+
+# Run a command
+do_cmd()
+{
+    local doer=''
+    if [ "${DRY_RUN}" = '1' ]; then
+        doer='echo'
+        if [ "$1" = '-quiet' ]; then
+            shift
+        fi
+    fi
+    if [ "$1" = '-ignore-error' ]; then
+        shift
+        $doer ${1+"$@"} 2>/dev/null || true
+    elif [ "$1" = '-quiet' ]; then
+        shift
+        $doer ${1+"$@"} >/dev/null
+    else
+        $doer ${1+"$@"}
+    fi
+}
+
+# Remove QoS settings
+clear_qos()
+{
+    do_cmd -ignore-error $TC qdisc del dev "$QOS_DEV_EXT" ingress
+    do_cmd -ignore-error $TC qdisc del dev "$QOS_DEV_EXT" root
+    do_cmd -ignore-error $TC qdisc del dev "$QOS_DEV_IFB" root
+}
+
+# Check devices really exist
+check_devices()
+{
+
+    # Check uplink interface
+    if ! [ -e /sys/class/net/"$QOS_DEV_EXT" ]; then
+        echo -n "$QOS_DEV_EXT not found"
+        return 1
+    fi
+
+    # Check ifb interface, loading module if necessary
+    if ! [ -e /sys/class/net/"$QOS_DEV_IFB" ]; then
+        $MODPROBE ifb
+        RESULT=$?
+        if [ $RESULT -ne 0 ]; then
+            echo -n "can't load module ifb"
+            return 1
+        fi
+        if ! [ -e /sys/class/net/"$QOS_DEV_IFB" ]; then
+            echo -n "$QOS_DEV_IFB not found"
+            return 1
+        fi
+    fi
+}
+
+# Add a tc filter
+add_filter()
+{
+    local dev="$1"
+    local parent="$2"
+    local flow="$3"
+    shift
+    shift
+    shift
+    do_cmd $TC filter add dev $dev parent $parent protocol ip prio 10 u32 ${1+"$@"} flowid $flow
+}
+
+#
+# Set up tc filters to classify high priority packets into the 1:10 class.
+#
+add_filters()
+{
+    # Get params
+    local dev="$1"
+    local parent="$2"
+    local flow="1:10"
+
+    # Small TCP packets (typically ACKs)
+    if [ "$QOS_TCP_ACK" = "yes" ]; then
+        add_filter $dev $parent $flow       \
+          match ip protocol 6 0xff          \
+          match u16 0x0000 0xffc0 at 2      \
+          match u8 0x10 0x10 at nexthdr+13
+    fi
+
+    # SSH interactive traffic
+    if [ "$QOS_SSH_INTERACTIVE" = "yes" ]; then
+        for OFFSET in 0 2; do
+            add_filter $dev $parent $flow       \
+              match ip tos 0x10 0x1c            \
+              match ip protocol 6 0xff        \
+              match u16 22 0xffff at nexthdr+$OFFSET
+        done
+    fi
+
+    # DNS traffic
+    if [ "$QOS_DNS" = "yes" ]; then
+        for PROTO in 6 17; do
+            for OFFSET in 0 2; do
+                add_filter $dev $parent $flow       \
+                  match ip protocol $PROTO 0xff     \
+                  match u16 53 0xffff at nexthdr+$OFFSET
+            done
+        done
+    fi
+
+    # User-specified filters
+    echo $QOS_HIGH_PRIORITY | tr ',' '\n' | while IFS="," read MATCH; do
+        if [ -n "$MATCH" ]; then
+            add_filter $dev $parent $flow $MATCH
+        fi
+    done
+}
+
+# Set up QoS
+set_qos()
+{
+    # Bring up ifb0
+    do_cmd $IP link set "$QOS_DEV_IFB" up
+
+    # Add ingress queueing discipline root
+    do_cmd $TC qdisc add dev "$QOS_DEV_EXT" ingress
+
+    # Configure outgoing traffic
+    set_qos_dev "$QOS_DEV_EXT" "$QOS_BANDWIDTH_UP_TOTAL" "$QOS_BANDWIDTH_UP_HIGHPRIO"
+
+    # Configure incoming traffic
+    set_qos_dev "$QOS_DEV_IFB" "$QOS_BANDWIDTH_DOWN_TOTAL" "$QOS_BANDWIDTH_DOWN_HIGHPRIO"
+
+    # Redirect all packets coming into the uplink to the ifb interface
+    do_cmd -quiet $TC filter add dev "$QOS_DEV_EXT" parent ffff: protocol ip prio 10 u32 \
+      match u32 0 0 flowid 1:1 action mirred egress redirect dev "$QOS_DEV_IFB"
+}
+
+
+# Add queue disciplines, classes, and filters for one interface
+set_qos_dev()
+{
+    # Get params
+    local dev="$1"
+    local total_bw="$2"
+    local hiprio_bw="$3"
+
+    # Define handles
+    local hiflow="1:10"
+    local loflow="1:20"
+
+    # Adjust total bandwidth to 98% of actual bandwidth
+    total_bw=$(( $total_bw * 98 / 100 ))
+
+    # Remainder is the low priority bandwidth
+    local loprio_bw=$(( $total_bw - $hiprio_bw ))
+
+    # Sanity check
+    if [ "$total_bw" -le 0 -o "$hiprio_bw" -le 0 -o "$loprio_bw" -le 0 \
+      -o "$hiprio_bw" -ge "$total_bw" -o "$loprio_bw" -ge "$total_bw" ]; then
+        echo "qos: illegal bandwidth settings on $dev" 1>&2
+        return 1
+    fi
+
+    ##
+    ## Set up HTB classes
+    ##
+
+    # Add the queueing discipline
+    do_cmd $TC qdisc add dev $dev root handle 1:0 htb default 20
+
+    # Add the root class
+    do_cmd $TC class add dev $dev parent 1:0 classid 1:1 htb \
+        rate ${total_bw}kbit ceil ${total_bw}kbit
+
+    # Add class for high priority packets
+    do_cmd $TC class add dev $dev parent 1:1 classid $hiflow htb \
+        rate ${hiprio_bw}kbit ceil ${total_bw}kbit prio 0 quantum 3000
+
+    # Add class for low priority packets
+    do_cmd $TC class add dev $dev parent 1:1 classid $loflow htb \
+        rate ${loprio_bw}kbit ceil ${total_bw}kbit prio 1 quantum 3000
+
+    ##
+    ## Set up filters to classify packets into the above classes
+    ##
+
+    add_filters $dev 1:0 $hiflow
+}
+
+# Watch QOS filter or class activity
+watch_qos()
+{
+    # Whether to watch filter or class
+    case "$2" in
+        -f)
+            watch -n 0.2 tc -p -s -d filter show dev "$QOS_DEV_IFB"
+            ;;
+        -c|*)
+            watch -n 0.2 tc -s class show dev "$QOS_DEV_IFB"
+            ;;
+    esac
+}
+
+# Main entry
+case "$1" in
+    start)
+        echo -n "Starting QoS "
+        if ! check_devices; then
+            rc_status -u
+        else
+            clear_qos
+            set_qos
+            rc_status -v
+        fi
+        ;;
+    stop)
+        echo -n "Stopping QoS "
+        clear_qos || true
+        rc_status -v
+        ;;
+    restart)
+        $0 stop
+        $0 start
+        ;;
+    show)
+        DRY_RUN="1"
+        clear_qos
+        set_qos
+        ;;
+    watch)
+        watch_qos ${1+"$@"}
+        ;;
+    *)
+        echo "Usage: $0 {start|stop|show|watch|restart}"
+        exit 1
+        ;;
+esac
+
+# Set exit status
+rc_exit
+
+# vim: sw=4
